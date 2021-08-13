263dcf585f0b99dda49c29604f4ec19c2814d6d3
mlgvla <mlgerardvla@gmail.com>
2021-08-13 15:29:08 -0400

    switched to class componenet to use componentDidMount

--- src/App.js
+++ src/App.js
@@ -1,28 +1,45 @@
-
 import './App.css';
 import { Route } from 'react-router-dom';
+import { connect } from 'react-redux'
 import Home from './components/Home';
 import RecipeSearchContainer from './containers/RecipeSearchContainer';
 import MyRecipesContainer from './containers/MyRecipesContainer';
 import MainHeader from './components/MainHeader';
 import RecipeDetail from './components/RecipeDetail';
+import { Component } from 'react';
 
 
+class App extends Component {
+   
+    componentDidMount() {
+        console.log(this.props)
+        //this.props.getMyRecipes()
+    }
 
-function App() {
-  return (
-    <div className="App">
-        <MainHeader />
-        <main>
-            <Route exact path='/'><Home /></Route>
-            <Route exact path='/search'><RecipeSearchContainer /></Route>
-            {/* will need a unique id for each hit - do that in the reducer */}
-            <Route path='/search/:recipeId'><RecipeDetail /></Route>
-            <Route exact path='/myrecipes'><MyRecipesContainer /></Route>
-        </main>
-        
-    </div>
-  );
+    render() {
+        return (
+            <div className="App">
+                <MainHeader />
+                <main>
+                    <Route exact path='/'><Home /></Route>
+                    <Route exact path='/search'><RecipeSearchContainer /></Route>
+                    {/* will need a unique id for each hit - do that in the reducer */}
+                    <Route path='/search/:recipeId'><RecipeDetail /></Route>
+                    <Route exact path='/myrecipes'><MyRecipesContainer /></Route>
+                </main>
+                
+            </div>
+        )
+    }
+ 
 }
 
-export default App;
+const mapStateToProps = state => {
+    return {}
+}
+
+const mapDispatchToProps = dispath => {
+    return {}
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(App);
