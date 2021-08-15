1583035e9e284ec287e62803d09bcca9a082666f
mlgvla <mlgerardvla@gmail.com>
2021-08-14 20:23:42 -0400

    mapped recipeHit to recipe for POST

--- src/components/recipe_search/RecipeCard.js
+++ src/components/recipe_search/RecipeCard.js
@@ -4,9 +4,25 @@ import { Link } from 'react-router-dom';
 const RecipeCard = (props) => {
     
     const recipeId = props.recipeHit.recipeId
+    const recipeHit = props.recipeHit.recipe
+
+    const handleClick = () => {
+    
+        let mappedRecipe = {
+            label: recipeHit.label,
+            image: recipeHit.image,
+            url: recipeHit.url,
+            yield: recipeHit.yield,
+            calories: recipeHit.calories,
+            ingredientLines: JSON.stringify(recipeHit.ingredientLines),
+            source: recipeHit.source
+        }
+        //props.saveRecipe(mappedRecipe)
+    }
     return (
         <div>
             <Link to={`/search/${recipeId}`}>{props.recipeHit.recipe.label}</Link>
+            <button onClick={handleClick} >Save</button>
         </div>
     );
 }
