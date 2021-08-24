8b4748092a475ded86ea978a86cc5bc9e6a0beaa
mlgvla <mlgerardvla@gmail.com>
2021-08-24 00:00:52 -0400

    fixed navbar

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,12 +1,19 @@
 import React, { Component } from 'react';
+// import { Link } from 'react-router-dom'
+import './Home.css'
 
 
 class Home extends Component {
     render() {
         return (
             <div>
-              <h1>Welcome to Mahlzeit!</h1>
+                
+
+            
             </div>
+            
+      
+           
         );
     }
 }
